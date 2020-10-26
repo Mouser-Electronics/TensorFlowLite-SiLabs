62848eb9180c940443ced16ca132a639c0e790bb
mike parks <mike@greenshoegarage.com>
2020-10-26 14:08:57 -0400

    control RGB LEDs

--- Software/src/command_responder.cc
+++ Software/src/command_responder.cc
@@ -31,6 +31,39 @@
  * Silicon Labs may update projects from time to time.
  ******************************************************************************/
 
+/******************************************************************************
+ * # License
+ * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * SPDX-License-Identifier: Zlib
+ *
+ * The licensor of this software is Silicon Laboratories Inc.
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ *******************************************************************************
+ * # Experimental Quality
+ * This code has not been formally tested and is provided as-is. It is not
+ * suitable for production environments. In addition, this code will not be
+ * maintained and there may be no bug maintenance planned for these resources.
+ * Silicon Labs may update projects from time to time.
+ ******************************************************************************/
+
 #include <string.h>
 #include "tensorflow/lite/micro/examples/micro_speech/command_responder.h"
 #include "bsp.h"
@@ -43,10 +76,16 @@ void RespondToCommand(tflite::ErrorReporter* error_reporter,
                       uint8_t score, bool is_new_command) {
     static bool initialized = false;
 
+    uint32_t ledColor_Red = 1;
+    uint32_t ledColor_Green = 2;
+    uint32_t ledColor_Blue = 3;
+    uint32_t ledColor_Off = 0;
+
     if(!initialized)
     {
         initialized = true;
-        BSP_LedsInit();
+        GPIO_PinModeSet(BSP_GPIO_LED0_PORT, BSP_GPIO_LED0_PIN, gpioModePushPull, 0);
+        GPIO_PinModeSet(BSP_GPIO_LED1_PORT, BSP_GPIO_LED1_PIN, gpioModePushPull, 0);
     }
 
    //error_reporter->Report("%s %d", found_command, score);
@@ -58,23 +97,24 @@ void RespondToCommand(tflite::ErrorReporter* error_reporter,
 
         if(strcmp(found_command, "unknown") == 0)
         {
-            BSP_LedSet(0);
-            BSP_LedSet(1);
+            BSP_ExtLedSet(0, ledColor_Red);
+            BSP_ExtLedSet(1, ledColor_red);
         }
         else if(strcmp(found_command, "no") == 0)
         {
-            BSP_LedSet(0);
-            BSP_LedClear(1);
+            BSP_ExtLedSet(0, ledColor_Green);
+            BSP_ExtLedSet(1, ledColor_Red);
         }
         else if(strcmp(found_command, "yes") == 0)
         {
-            BSP_LedClear(0);
-            BSP_LedSet(1);
+            BSP_ExtLedSet(0, ledColor_Red);
+            BSP_ExtLedSet(1, ledColor_Green);
         }
         else if(strcmp(found_command, "silence") == 0)
         {
-            BSP_LedClear(0);
-            BSP_LedClear(1);
+            // LEDs Off, the multicolored LEDs on the GG11 work off negative logic
+            BSP_ExtLedSet(0, ledColor_Off);
+            BSP_ExtLedSet(1, ledColor_Off);
         }
   }
 }
